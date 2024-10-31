2df719b4f4c6a9b58f5b6040a9e5e51dff29dce9
emrahelis <emrahelis@gmail.com>
2024-10-31 19:41:53 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+// SortedDictionary koleksiyonu, dictionary koleksiyonun key değerine göre koleksiyonu sıralayan versiyonudur!!!
+
+
+// Tanımlama : 
+
+SortedDictionary<int,string> dictionary = new SortedDictionary<int, string>();
+
+dictionary.Add(5, "Ahmet");
+dictionary.Add(2, "Hasan");
+dictionary.Add(-1,"Yıldız");
+dictionary.Add(-1,"ipek");
+
+
+// Ekrana yazdıralım 
+
+foreach(KeyValuePair<int,string> item in dictionary){
+
+    Console.WriteLine("Key : {0}",item.Key);
+    Console.WriteLine("Value : {0}",item.Value);
+}
